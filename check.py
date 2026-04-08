8769d1b43f0bb3cc07869265d6671cb1e7e98cd7
yashshinde0080 <syash0080@gmail.com>
2026-04-08 22:52:48 +0530

    8/4/2026

--- check.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('frontend/gradio_ui.py', 'r', encoding='utf-8') as f:
-    content = f.read()
-styles = re.findall(r"style='([^']+)'" + "|" + r"style=\"([^\"]+)\"", content)
-flat_styles = [s[0] or s[1] for s in styles]
-for s in sorted(set(flat_styles)):
-    print(s) 
